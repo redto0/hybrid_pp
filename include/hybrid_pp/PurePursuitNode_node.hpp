310e88dd341a3c335757b649ea7242c85513fcb0
im-a-robo <nmdas05x@gmail.com>
2023-10-13 19:51:09 -0400

    working steering angle with goal_pose from rviz #1

--- include/hybrid_pp/PurePursuitNode_node.hpp
+++ include/hybrid_pp/PurePursuitNode_node.hpp
@@ -5,11 +5,44 @@
 #include "ackermann_msgs/msg/ackermann_drive.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
+#include <tf2/exceptions.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+
 class PurePursuitNode : public rclcpp::Node {
 private:
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub;
 
-    rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>::SharedPtr pub;
+    // Var
+    float look_ahead_distance_;
+    float target_point_x_, target_point_y_;
+    float alpha_; 
+    geometry_msgs::msg::PoseStamped current_pose_;
+    geometry_msgs::msg::TransformStamped transformStamped_;
+
+
+    // Parameters
+    tf2::Duration transform_tolerance_;
+    float min_look_ahead_distance_;
+    float max_look_ahead_distance_;
+    float k_dd_;
+    float speed_;
+    std::string robot_frame_;
+    std::string map_frame_;
+    void update_params();
+
+    std::string rear_axle_frame_;
+    std::string odom_frame_;
+    rclcpp::TimerBase::SharedPtr param_update_timer_;
+
+    // TF
+    std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+
+    // Pub Sub
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>::SharedPtr nav_ack_vel_pub_;
 
 public:
     PurePursuitNode(const rclcpp::NodeOptions& options);
--- src/PurePursuitNode_node.cpp
+++ src/PurePursuitNode_node.cpp
@@ -1,31 +1,80 @@
 #include "hybrid_pp/PurePursuitNode_node.hpp"
 
+#include <algorithm> 
+#include <cmath>
+
+
 // For _1
 using namespace std::placeholders;
 
+// For 1000ms
+using namespace std::chrono_literals;
+
 PurePursuitNode::PurePursuitNode(const rclcpp::NodeOptions& options) : Node("PurePursuitNode", options) {
-    // Parameters
-    float x = this->declare_parameter<float>("foo", -10.0);
+    // Params (tune theses)
+    min_look_ahead_distance_ = 0.0;
+    max_look_ahead_distance_ = 1.0;
+    k_dd_ = 1.0;
+    speed_ = 1.0;
 
     // Pub Sub
-    sub = this->create_subscription<geometry_msgs::msg::PoseStamped>("/goal_pose", 1, std::bind(&PurePursuitNode::ackerman_cb, this, _1));
-    pub = this->create_publisher<ackermann_msgs::msg::AckermannDrive>("/nav_ack_vel", 1);
+    goal_pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/goal_pose", 1, std::bind(&PurePursuitNode::ackerman_cb, this, _1));
+    nav_ack_vel_pub_ = this->create_publisher<ackermann_msgs::msg::AckermannDrive>("/nav_ack_vel", 1);
 
-    // Log a sample log
-    RCLCPP_INFO(this->get_logger(), "You passed %f", x);
+    // TF
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+    // Initial var state
+    transform_tolerance_ = tf2::durationFromSec(0.1);
 
 }
 
+// base_link to odom
 void PurePursuitNode::ackerman_cb(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
     // Log a sample log
-    RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->pose.position.x);
+    RCLCPP_INFO(this->get_logger(), "I heard the message");
 
-    // Create a message to publish
-    auto ack_msg = ackermann_msgs::msg::AckermannDrive();
-    ack_msg.steering_angle = 0.0;
-    ack_msg.speed = 0.0;
+    geometry_msgs::msg::PoseWithCovarianceStamped robot_pose;
+    geometry_msgs::msg::PoseWithCovarianceStamped msg_to_goal_pose;
+    geometry_msgs::msg::PoseWithCovarianceStamped transformed_robot_pose;
+    geometry_msgs::msg::PoseWithCovarianceStamped transformed_goal_pose;
 
+    msg_to_goal_pose.header.frame_id = msg->header.frame_id;
+    msg_to_goal_pose.header.stamp = this->get_clock()->now();
+    msg_to_goal_pose.pose.pose.position.x = msg->pose.position.x;
+    msg_to_goal_pose.pose.pose.position.y = msg->pose.position.y;
+    msg_to_goal_pose.pose.pose.position.z = msg->pose.position.z;
+
+    transformed_goal_pose = tf_buffer_->transform(msg_to_goal_pose, "rear_axle", transform_tolerance_);
+
+    // Calc look ahead distance 
+    look_ahead_distance_ = std::clamp(k_dd_ * speed_, min_look_ahead_distance_, max_look_ahead_distance_);
+
+    RCLCPP_INFO(this->get_logger(), "Robot pose x: '%f'", transformed_robot_pose.pose.pose.position.x);
+    RCLCPP_INFO(this->get_logger(), "Robot pose y: '%f'", transformed_robot_pose.pose.pose.position.y);
+    RCLCPP_INFO(this->get_logger(), "Goal pose x: '%f'", transformed_goal_pose.pose.pose.position.x);
+    RCLCPP_INFO(this->get_logger(), "Goal pose y: '%f'", transformed_goal_pose.pose.pose.position.y);
+
+    // distance function that works to geometry_msgs
+    auto distance = [](const geometry_msgs::msg::Point& p1, const geometry_msgs::msg::Point& p2) {
+        return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
+    };
+    
+    float steering_angle_;
+
+    // Calculate the angle between the robot and the goal
+    alpha_ = std::atan2(transformed_goal_pose.pose.pose.position.y - transformed_robot_pose.pose.pose.position.y, transformed_goal_pose.pose.pose.position.x - transformed_robot_pose.pose.pose.position.x);
+
+    // Calculate the steering angle (needs wheel base) TODO
+    steering_angle_ = std::atan(2.0 * 1.08 * std::sin(alpha_) / look_ahead_distance_);
+
+    // Create the ackermann message
+    ackermann_msgs::msg::AckermannDrive ack_msg;
+    ack_msg.steering_angle = steering_angle_;
+    ack_msg.speed = speed_;
+    
     // Publish the message
-    pub->publish(ack_msg);
+    nav_ack_vel_pub_->publish(ack_msg);
     
 }
