eabb31007448d8bae934a6eedc1da1c490d78cd2
im-a-robo <nmdas05x@gmail.com>
2023-10-25 00:39:32 -0400

    working on getting a marker to show in rviz, not working yet #1

--- include/hybrid_pp/PurePursuitNode_node.hpp
+++ include/hybrid_pp/PurePursuitNode_node.hpp
@@ -10,35 +10,38 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 
-class PurePursuitNode : public rclcpp::Node {
+class PurePursuitNode : public rclcpp::Node
+{
 private:
-    // Var
-    float look_ahead_distance_;
-    float target_point_x_, target_point_y_;
-    float alpha_;
-    float speed_;
+  // Var
+  float look_ahead_distance_;
+  float target_point_x_, target_point_y_;
+  float alpha_;
+  float speed_;
 
-    // Parameters
-    tf2::Duration transform_tolerance_;
-    float min_look_ahead_distance_;
-    float max_look_ahead_distance_;
-    float k_dd_;
-    std::string rear_axle_frame_;
+  // Parameters
+  tf2::Duration transform_tolerance_;
+  float min_look_ahead_distance_;
+  float max_look_ahead_distance_;
+  float k_dd_;
+  std::string rear_axle_frame_;
 
-    // TF
-    std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  // TF
+  std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 
-    // Pub Sub
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
-    rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr odom_ack_sub_;
-    rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>::SharedPtr nav_ack_vel_pub_;
+  // Pub Sub
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
+  rclcpp::Subscription<ackermann_msgs::msg::AckermannDrive>::SharedPtr odom_ack_sub_;
+  rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>::SharedPtr nav_ack_vel_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr path_vis_marker_pub_;
 
 public:
-    PurePursuitNode(const rclcpp::NodeOptions& options);
+  PurePursuitNode(const rclcpp::NodeOptions & options);
 
-    /// subscriber callback
-    void ackerman_cb(geometry_msgs::msg::PoseStamped::SharedPtr msg);
-    void odom_speed_cb(ackermann_msgs::msg::AckermannDrive::SharedPtr msg);
+  /// subscriber callback
+  void ackerman_cb(geometry_msgs::msg::PoseStamped::SharedPtr msg);
+  void odom_speed_cb(ackermann_msgs::msg::AckermannDrive::SharedPtr msg);
 };
